ba15c5c3fcf82d89ed48fa1cf652615d1627e409
Robin Laakso <robin.laakso1@gmail.com>
2025-02-19 10:11:52 +0100

    Update

--- src/pages/Mattlaggning.jsx
+++ src/pages/Mattlaggning.jsx
@@ -7,35 +7,33 @@ const tjänster = [
     src: badrumImage,
   },
   {
-    name: "Mattläggning",
-    text: "",
+    name: "Kök",
+    text: "I köket är det viktigt att ha en slitstark och lättskött golvyta. Plastmattor erbjuder en både estetisk och praktisk lösning för köksgolv.",
     src: badrumImage,
   },
 ];
 
 const Mattlaggning = () => {
   return (
-    <div className="relative w-full min-h-screen">
-      <div className="inset-0 flex justify-center items-center pt-60">
-        <div className="text-center text-white">
-          <div className="flex flex-wrap justify-center gap-6">
-            {tjänster.map((service, index) => (
-              <div
-                key={index}
-                className="w-full sm:w-1/2 md:w-1/3 bg-transparent"
-              >
-                <img
-                  src={service.src}
-                  alt={service.name}
-                  className="w-full h-70 object-cover rounded-md mb-4"
-                />
-                <h2 className="text-2xl font-bold mb-2">{service.name}</h2>
-                <p className="text-base">{service.text}</p>
-              </div>
-            ))}
+    <div className="text-white relative w-full min-h-screen flex flex-col justify-center items-center pt-60 pb-10 space-y-16">
+      {tjänster.map((service, index) => (
+        <div
+          key={index}
+          className={`w-3/4 flex flex-col md:flex-row items-center bg-transparent ${
+            index % 2 === 0 ? "md:flex-row" : "md:flex-row-reverse"
+          }`}
+        >
+          <img
+            src={service.src}
+            alt={service.name}
+            className="w-full md:w-1/2 h-70 object-cover rounded-md"
+          />
+          <div className="w-full md:w-1/2 p-6 text-left">
+            <h2 className="text-2xl font-bold mb-2">{service.name}</h2>
+            <p className="text-base">{service.text}</p>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
